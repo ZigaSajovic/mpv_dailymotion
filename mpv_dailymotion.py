7a8c28fd0a1fe4cfb7377a2986c5a29ee9ae3f6a
ZigaSajovic <ziga.sajovic@gmail.com>
2019-10-10 19:23:39 +0200

    skip if not a dailymotion link

--- mpv_dailymotion.py
+++ mpv_dailymotion.py
@@ -19,28 +19,31 @@ if len(args) == 0:
   exit(-1)
 
 link = args[0]
-"""
-The first call will fail, providing url which it attempted to
-access.
-Failed to open .....
-We capture that url.
-"""
-result = subprocess.run(["mpv", link], stdout=subprocess.PIPE)
-filtered = list(filter(lambda x: "Failed to open" in x,
-                       str(result.stdout).split("\\n")))
-if len(filtered) != 1:
-  print("Something went wrong")
-  exit(-1)
-id_ = filtered[0].find("http")
-link = filtered[0][id_:-1]
 
-"""
-We get the header of the request of the captured url,
-and extract the appropriate location, and modify it,
-so that it works with mpv.
-"""
-true_link = requests.head(link).headers["Location"]
-del_from = true_link.find("#cell=")
-true_link = true_link[0:del_from]
+#If its not a dailymotion link, skip the processing
+if "dailymotion" in link:
+  """
+  The first call will fail, providing url which it attempted to
+  access.
+  Failed to open .....
+  We capture that url.
+  """
+  result = subprocess.run(["mpv", link], stdout=subprocess.PIPE)
+  filtered = list(filter(lambda x: "Failed to open" in x,
+                         str(result.stdout).split("\\n")))
+  if len(filtered) != 1:
+    print("Something went wrong")
+    exit(-1)
+  id_ = filtered[0].find("http")
+  link = filtered[0][id_:-1]
 
-os.system(" ".join(["mpv", '"' + true_link + '"'] + args[1:]))
+  """
+  We get the header of the request of the captured url,
+  and extract the appropriate location, and modify it,
+  so that it works with mpv.
+  """
+  true_link = requests.head(link).headers["Location"]
+  del_from = true_link.find("#cell=")
+  link = true_link[0:del_from]
+
+os.system(" ".join(["mpv", '"' + link + '"'] + args[1:]))
