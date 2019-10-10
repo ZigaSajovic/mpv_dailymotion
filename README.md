d2f31cdeb84ade49db3294d3bc2cf5aaf5383be0
ZigaSajovic <ziga.sajovic@gmail.com>
2019-10-10 18:19:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# mpv_dailymotion
+Stream videos from dailymotion using mpv
+
+There is a known problem with playing videos from dailymotion using mpv, due to ffmpeg. This script gets around those problems.
+
+## Usage
+
+```sh
+mpv_dailymotion.py <link to video> <other> <args> <to forward> <to mpv>
+``` 
---
+++ mpv_dailymotion.py
@@ -0,0 +1,44 @@
+#/bin/python3
+"""
+This script will play a video from dailymotion,
+using mpv player. It gets around a problem
+with ffmpeg and mpv.
+
+The first argument
+it expects is a link, the rest will be
+forwarded to mpv.
+"""
+import requests
+import subprocess
+import sys
+
+args = sys.argv[1:]
+if len(args) == 0:
+  print("Provide a link.")
+  exit(-1)
+
+link = args[0]
+"""
+The first call will fail, providing url which it attempted to
+access.
+Failed to open .....
+We capture that url.
+"""
+result = subprocess.run(["mpv", link], stdout=subprocess.PIPE)
+filtered = list(filter(lambda x: "Failed to open" in x,
+                       str(result.stdout).split("\\n")))
+if len(filtered) != 1:
+  print("Something went wrong")
+  exit(-1)
+id_ = filtered[0].find("http")
+link = filtered[0][id_:-1]
+
+"""
+We get the header of the request of the captured url,
+and extract the appropriate location, and modify it,
+so that it works with mpv.
+"""
+true_link = requests.head(link).headers["Location"]
+del_from = true_link.find("#cell=")
+true_link = true_link[0:del_from]
+subprocess.run(["mpv", true_link, " ".join(args[1:])])
